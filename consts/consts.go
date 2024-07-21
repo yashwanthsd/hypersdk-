0b93f1e29ff74f8496d5e53fe0b0fb8f7aa7b673
Rahul Berwal <tobiasrahul58@gmail.com>
2024-07-21 19:31:00 +0530

    commiting

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "RAHULHRP"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "RAHUL"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "RB"
 )
 
 var ID ids.ID
