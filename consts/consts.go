45c7bdbc40c75c592d71b03d0d6adfca5bc46c7d
yashwanthsd <yashwanths.sd@gmail.com>
2024-12-21 15:51:06 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "blockchain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "block"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "BC"
 )
 
 var ID ids.ID
